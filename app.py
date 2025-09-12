bafdb37021666d3fedda88d394b415add81af1df
IlogonPearl <118979116+IlogonPearl@users.noreply.github.com>
2025-09-12 13:40:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ st.subheader("📈 Sales Report by Category")
 sales_df = load_sales()
 
 if not sales_df.empty:
-    items_to_category = {
+    item_to_category = {
         item: cat
         for cat, items in menu_data.items()
         for item in items.keys()
@@ -231,6 +231,7 @@ if not sales_df.empty:
     st.pyplot(fig, use_container_width=False)
 else:
     st.info("No sales recorded yet.")
+
 
 
 
